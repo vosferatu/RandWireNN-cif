ef765d6c2020d74decea6ec9ee90253d47540351
vosferatu <joaomendessousela@gmail.com>
2020-05-10 17:49:43 +0200

    testing

--- reporting/evolution.py
+++ reporting/evolution.py
@@ -1,5 +1,5 @@
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 def plot_evolution(seed):
@@ -22,5 +22,52 @@ def plot_evolution(seed):
     plt.clf()
 
 
-for i in range(1, 11):
-    plot_evolution(i)
+def plot_dataset(dataset):
+    ax = plt.gca()
+
+    random_weights = pd.read_csv(f'c_78_p_0.75_graph_WS_dataset_{dataset}_seed_-1_name_first_gen_opt_SGD0.csv')
+    sgd_default_data = pd.read_csv(f'c_78_p_0.75_graph_WS_dataset_{dataset}_seed_-1_name_first_gen_opt_SGD1.csv')
+    sgd_no_momentum_data = pd.read_csv(
+        f'c_78_p_0.75_graph_WS_dataset_{dataset}_seed_-1_name_first_gen_opt_SGD_NO_MOMENTUM1.csv')
+
+    random_weights.plot(kind='line', x='epoch', y='accuracy', ax=ax, title=f'Accuracy evolution {dataset}', legend=True)
+    sgd_default_data.plot(kind='line', x='epoch', y='accuracy', ax=ax, title=f'Accuracy evolution {dataset}',
+                          legend=True)
+    sgd_no_momentum_data.plot(kind='line', x='epoch', y='accuracy', ax=ax, title=f'Accuracy evolution {dataset}',
+                              legend=True)
+
+    ax.legend(['random_weights', 'predicted_sgd_default', 'predicted_sgd_no_momentum'])
+
+    plt.savefig(f'./plots/first_gen_{dataset}.png')
+
+    plt.clf()
+
+
+def plot_optimizer(opt):
+    ax = plt.gca()
+
+    mnist = pd.read_csv(f'c_78_p_0.75_graph_WS_dataset_MNIST_seed_-1_name_first_gen_opt_{opt}.csv')
+    fashion_mnist = pd.read_csv(f'c_78_p_0.75_graph_WS_dataset_FASHION_MNIST_seed_-1_name_first_gen_opt_{opt}.csv')
+    cifar10 = pd.read_csv(f'c_78_p_0.75_graph_WS_dataset_CIFAR10_seed_-1_name_first_gen_opt_{opt}.csv')
+    cifar100 = pd.read_csv(f'c_78_p_0.75_graph_WS_dataset_CIFAR100_seed_-1_name_first_gen_opt_{opt}.csv')
+
+    mnist.plot(kind='line', x='epoch', y='accuracy', ax=ax, title=f'Accuracy evolution {opt}', legend=True)
+    fashion_mnist.plot(kind='line', x='epoch', y='accuracy', ax=ax, title=f'Accuracy evolution {opt}', legend=True)
+    cifar10.plot(kind='line', x='epoch', y='accuracy', ax=ax, title=f'Accuracy evolution {opt}', legend=True)
+    cifar100.plot(kind='line', x='epoch', y='accuracy', ax=ax, title=f'Accuracy evolution {opt}', legend=True)
+
+    ax.legend(['mnist', 'fashion_mnist', 'cifar10', 'cifar100'])
+
+    plt.savefig(f'./plots/first_gen_{opt}.png')
+
+    plt.clf()
+
+
+datasets = ['MNIST', 'FASHION_MNIST', 'CIFAR10', 'CIFAR100']
+optimizers = ['SGD0', 'SGD0', 'SGD_NO_MOMENTUM1']
+
+for i in datasets:
+    plot_dataset(i)
+
+for i in optimizers:
+    plot_optimizer(i)
